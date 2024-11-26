2daaaec521fcfa152a85fa406271c3abdad8a5db
Santi <zanti.f@gmail.com>
2024-11-26 12:32:03 -0300

    react error

---
+++ README.md
@@ -0,0 +1,15 @@
+# Despliegue de Aplicación Django y React.js
+
+Este repositorio contiene el código para desplegar una aplicación Django (backend) y React.js (frontend) usando Docker Compose.
+
+## Requisitos
+
+- Docker
+- Docker Compose
+
+## Instrucciones de despliegue local
+
+1. Clona este repositorio:
+   ```bash
+   git clone <URL_DEL_REPOSITORIO>
+   cd <NOMBRE_DEL_REPOSITORIO>
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,19 +11,19 @@ then
     echo "PostgreSQL started"
 fi
 
-# Uncomment below to flush db e.g. after running tests
-# Just make sure you really mean it 
-# python manage.py flush --no-input
-
-# We have base custom user model so need to makemigrations out of box
-
+# Crear migraciones necesarias
 python manage.py makemigrations core
-python manage.py flush --no-input
+
+# Aplicar migraciones a la base de datos
 python manage.py migrate
-python manage.py loaddata initial_data.json
-# python manage.py collectstatic --noinput
+
+# Cargar datos iniciales si es necesario
+# python manage.py loaddata initial_data.json
+
+# Colectar archivos estáticos
+python manage.py collectstatic --noinput
+
+# Iniciar el servidor Django
 python manage.py runserver 0.0.0.0:8000
-
-
 
 exec "$@"
